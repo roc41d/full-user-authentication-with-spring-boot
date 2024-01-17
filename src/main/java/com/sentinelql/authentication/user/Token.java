ab491c33c60766fa41091fa26c1dd89201ffc442
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-17 19:26:21 +0100

    (feature: token-confirmation-mechanism): create token entity

---
+++ src/main/java/com/sentinelql/authentication/user/Token.java
@@ -0,0 +1,29 @@
+package com.sentinelql.authentication.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne // many tokens can belong to one user
+    @JoinColumn(nullable = false, name = "userId")
+    private User user;
+}
